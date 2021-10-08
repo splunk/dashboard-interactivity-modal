5021d962ea699e1128de3b78ca60d33b7e8a1ea6
Jonathan Dillman <jdillman@splunk.com>
2021-10-08 09:02:25 -0700

    put the files in the right place

--- packages/modal-dashboard/src/main/webapp/pages/dashboard/index.jsx
+++ packages/modal-dashboard/src/main/webapp/pages/dashboard/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import layout from '@splunk/react-page';
-import DashboardExample from '../../../DashboardExample';
+import DashboardExample from './DashboardExample';
 
 layout(<DashboardExample />, {
     pageTitle: 'Custom Modal',
