c5ce28c4bcad27c838c19ddad5dd499363c117b9
Jonathan Dillman <jdillman@splunk.com>
2021-10-15 07:53:08 -0700

    filter the table data based off region

--- packages/modal-dashboard/src/main/webapp/pages/dashboard/DataModal.jsx
+++ packages/modal-dashboard/src/main/webapp/pages/dashboard/DataModal.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Modal from '@splunk/react-ui/Modal';
 
-import Tab from './Tab';
+import DataTable from './DataTable';
 
-const DataModal = ({ handleRequestClose, modalTitle, open }) => {
+const DataModal = ({ handleRequestClose, region, open }) => {
     return (
         <Modal onRequestClose={handleRequestClose} open={open} style={{ width: '640px' }}>
-            <Modal.Header title={modalTitle} onRequestClose={handleRequestClose} />
+            <Modal.Header title={`${region} region cheaters`} onRequestClose={handleRequestClose} />
             <Modal.Body style={{ padding: 10 }}>
-                <Tab />
+                <DataTable region={region} />
             </Modal.Body>
         </Modal>
     );
